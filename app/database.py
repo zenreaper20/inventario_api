277b73fcec11fb891fc242636ebdeea853f99895
Alejandro Alberto Juárez <dalex@gmail.com>
2026-03-16 21:12:14 -0600

    add postgres support

--- app/database.py
+++ app/database.py
@@ -1,15 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-# Base de datos SQLite
-SQLALCHEMY_DATABASE_URL = "sqlite:///./inventario.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+if not DATABASE_URL:
+    DATABASE_URL = "postgresql://postgres:WuZyYggoDexXRzskbEpIAYcpPFQivsVb@postgres.railway.internal:5432/railway"
+
+
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args = {"check_same_thread": False}
+
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    DATABASE_URL,
+    connect_args=connect_args
 )
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+SessionLocal = sessionmaker(autocommit=False,autoflush=False,bind=engine)
 Base = declarative_base()
 
 def get_db():
