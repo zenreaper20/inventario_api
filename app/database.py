e205f70299a3fbd39548ac3f2ed7c88139ef339a
Alejandro Alberto Juárez <dalex@gmail.com>
2026-03-18 16:50:46 -0600

    fix bcrypt error

--- app/database.py
+++ app/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+print("DATABASE_URL:", DATABASE_URL)
 
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
